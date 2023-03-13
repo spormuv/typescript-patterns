0c3fdc0c28a9f6d3d834ec9cff5dd54e12105044
Mike <spormuv@gmail.com>
2023-03-13 12:20:08 +0300

    feat: add bridge pattern

---
+++ structural_patterns/bridge.ts
@@ -0,0 +1,53 @@
+interface IProvider {
+  sendMessage(message: string): void;
+  connect(config: unknown): void;
+  disconnect(): void;
+}
+
+class TelegramProvider implements IProvider {
+  sendMessage(message: string): void {
+    console.log(message);
+  }
+  connect(config: string): void {
+    console.log(config);
+  }
+  disconnect(): void {
+    console.log('Disconnected TG');
+  }
+}
+
+class WhatsUpProvider implements IProvider {
+  sendMessage(message: string): void {
+    console.log(message);
+  }
+  connect(config: string): void {
+    console.log(config);
+  }
+  disconnect(): void {
+    console.log('Disconnected WU');
+  }
+}
+
+class NotificationSender {
+  constructor(private provider: IProvider) {}
+
+  send() {
+    this.provider.connect('connect');
+    this.provider.sendMessage('message');
+    this.provider.disconnect();
+  }
+}
+
+class DelayNotificationSender extends NotificationSender {
+  constructor(provider: IProvider) {
+    super(provider);
+  }
+
+  sendDelayed() {}
+}
+
+// implementation
+const sender = new NotificationSender(new TelegramProvider());
+sender.send();
+const sender2 = new NotificationSender(new WhatsUpProvider());
+sender2.send();
