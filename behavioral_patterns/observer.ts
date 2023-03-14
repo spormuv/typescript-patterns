a2ecdec66dbec45265a1795966d7fc1b937970d7
Mike <spormuv@gmail.com>
2023-03-14 15:11:10 +0300

    feat: add observer pattern

---
+++ behavioral_patterns/observer.ts
@@ -0,0 +1,67 @@
+interface Observer {
+  update(subject: Subject): void;
+}
+
+interface Subject {
+  attach(observer: Observer): void;
+  detach(observer: Observer): void;
+  notify(): void;
+}
+
+class Lead {
+  constructor(public name: string, public phone: string) {}
+}
+
+class NewLead implements Subject {
+  private observers: Observer[] = [];
+  public state: Lead;
+
+  attach(observer: Observer): void {
+    if (this.observers.includes(observer)) {
+      return;
+    }
+    this.observers.push(observer);
+  }
+
+  detach(observer: Observer): void {
+    const observerIndex = this.observers.indexOf(observer);
+    if (observerIndex === -1) {
+      return;
+    }
+    this.observers.splice(observerIndex, 1);
+  }
+
+  notify(): void {
+    for (const observer of this.observers) {
+      observer.update(this);
+    }
+  }
+}
+
+class NotificationService implements Observer {
+  update(subject: Subject): void {
+    console.log('NotificationService has received notification');
+    console.log(subject);
+  }
+}
+
+class LeadService implements Observer {
+  update(subject: Subject): void {
+    console.log('LeadService has received notification');
+    console.log(subject);
+  }
+}
+
+// implementation
+const subject = new NewLead();
+subject.state = new Lead('Name', '+001122');
+
+const s1 = new NotificationService();
+const s2 = new LeadService();
+
+subject.attach(s1);
+subject.attach(s2);
+
+subject.notify();
+subject.detach(s1);
+subject.notify();
---
+++ build/behavioral_patterns/observer.js
@@ -0,0 +1,52 @@
+"use strict";
+class Lead {
+    constructor(name, phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
+class NewLead {
+    constructor() {
+        this.observers = [];
+    }
+    attach(observer) {
+        if (this.observers.includes(observer)) {
+            return;
+        }
+        this.observers.push(observer);
+    }
+    detach(observer) {
+        const observerIndex = this.observers.indexOf(observer);
+        if (observerIndex === -1) {
+            return;
+        }
+        this.observers.splice(observerIndex, 1);
+    }
+    notify() {
+        for (const observer of this.observers) {
+            observer.update(this);
+        }
+    }
+}
+class NotificationService {
+    update(subject) {
+        console.log('NotificationService has received notification');
+        console.log(subject);
+    }
+}
+class LeadService {
+    update(subject) {
+        console.log('LeadService has received notification');
+        console.log(subject);
+    }
+}
+// implementation
+const subject = new NewLead();
+subject.state = new Lead('Name', '+001122');
+const s1 = new NotificationService();
+const s2 = new LeadService();
+subject.attach(s1);
+subject.attach(s2);
+subject.notify();
+subject.detach(s1);
+subject.notify();
