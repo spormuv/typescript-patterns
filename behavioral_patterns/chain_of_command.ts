d4ebb858cade7f600b1c6fe3a67e7ef74363d2ec
Mike <spormuv@gmail.com>
2023-03-14 08:55:17 +0300

    feat: add chain pattern

---
+++ behavioral_patterns/chain_of_command.ts
@@ -0,0 +1,61 @@
+interface IMiddleware {
+  next(mid: IMiddleware): IMiddleware;
+  handle(request: any): any;
+}
+
+abstract class AbstractMiddleware implements IMiddleware {
+  private nextMiddleware: IMiddleware;
+
+  next(mid: IMiddleware): IMiddleware {
+    this.nextMiddleware = mid;
+    return mid;
+  }
+
+  handle(request: any) {
+    if (this.nextMiddleware) {
+      return this.nextMiddleware.handle(request);
+    }
+    return;
+  }
+}
+
+class AuthMiddleware extends AbstractMiddleware {
+  override handle(request: any) {
+    console.log('AuthMiddleware!');
+    if (request.userId === 1) {
+      return super.handle(request);
+    }
+    return { error: 'Not authorized!' };
+  }
+}
+
+class ValidateMiddleware extends AbstractMiddleware {
+  override handle(request: any) {
+    console.log('ValidateMiddleware!');
+    if (request.body) {
+      return super.handle(request);
+    }
+    return { error: 'No body!' };
+  }
+}
+
+class Controller extends AbstractMiddleware {
+  override handle(request: any) {
+    console.log('Controller!');
+    return { success: request };
+  }
+}
+
+// implementation
+const controller = new Controller();
+const validate = new ValidateMiddleware();
+const auth = new AuthMiddleware();
+
+auth.next(validate).next(controller);
+
+console.log(
+  auth.handle({
+    userId: 1,
+    body: 'test',
+  })
+);
