1d2b29cb3cdc3470e9af63d9b2edfad15150fc54
Mike <spormuv@gmail.com>
2023-03-14 10:27:56 +0300

    feat: add command pattern

---
+++ behavioral_patterns/command.ts
@@ -0,0 +1,79 @@
+class User {
+  constructor(public userId: number) {}
+}
+
+class CommandHistory {
+  public commands: Command[] = [];
+  push(command: Command) {
+    this.commands.push(command);
+  }
+  remove(command: Command) {
+    this.commands = this.commands.filter(
+      (c) => c.commandId !== command.commandId
+    );
+  }
+}
+
+abstract class Command {
+  public commandId: number;
+
+  abstract execute(): void;
+
+  constructor(public history: CommandHistory) {
+    this.commandId = Math.random();
+  }
+}
+
+class AddUserCommand extends Command {
+  constructor(
+    private user: User,
+    private receiver: UserService,
+    history: CommandHistory
+  ) {
+    super(history);
+  }
+
+  execute(): void {
+    this.receiver.saveUser(this.user);
+    this.history.push(this);
+  }
+
+  undo() {
+    this.receiver.deleteUser(this.user.userId);
+    this.history.remove(this);
+  }
+}
+
+class UserService {
+  saveUser(user: User) {
+    console.log(`Saving user with id ${user.userId}`);
+  }
+  deleteUser(userId: number) {
+    console.log(`Deleting user with id ${userId}`);
+  }
+}
+
+class Controllers {
+  receiver: UserService;
+  history: CommandHistory = new CommandHistory();
+
+  addReceiver(receiver: UserService) {
+    this.receiver = receiver;
+  }
+  run() {
+    const addUserCommand = new AddUserCommand(
+      new User(1),
+      this.receiver,
+      this.history
+    );
+    addUserCommand.execute();
+    console.log(addUserCommand.history);
+    addUserCommand.undo();
+    console.log(addUserCommand.history);
+  }
+}
+
+// implementation
+const controllers = new Controllers();
+controllers.addReceiver(new UserService());
+controllers.run();
---
+++ build/behavioral_patterns/command.js
@@ -0,0 +1,65 @@
+"use strict";
+class User {
+    constructor(userId) {
+        this.userId = userId;
+    }
+}
+class CommandHistory {
+    constructor() {
+        this.commands = [];
+    }
+    push(command) {
+        this.commands.push(command);
+    }
+    remove(command) {
+        this.commands = this.commands.filter((c) => c.commandId !== command.commandId);
+    }
+}
+class Command {
+    constructor(history) {
+        this.history = history;
+        this.commandId = Math.random();
+    }
+}
+class AddUserCommand extends Command {
+    constructor(user, receiver, history) {
+        super(history);
+        this.user = user;
+        this.receiver = receiver;
+    }
+    execute() {
+        this.receiver.saveUser(this.user);
+        this.history.push(this);
+    }
+    undo() {
+        this.receiver.deleteUser(this.user.userId);
+        this.history.remove(this);
+    }
+}
+class UserService {
+    saveUser(user) {
+        console.log(`Saving user with id ${user.userId}`);
+    }
+    deleteUser(userId) {
+        console.log(`Deleting user with id ${userId}`);
+    }
+}
+class Controllers {
+    constructor() {
+        this.history = new CommandHistory();
+    }
+    addReceiver(receiver) {
+        this.receiver = receiver;
+    }
+    run() {
+        const addUserCommand = new AddUserCommand(new User(1), this.receiver, this.history);
+        addUserCommand.execute();
+        console.log(addUserCommand.history);
+        addUserCommand.undo();
+        console.log(addUserCommand.history);
+    }
+}
+// implementation
+const controllers = new Controllers();
+controllers.addReceiver(new UserService());
+controllers.run();
